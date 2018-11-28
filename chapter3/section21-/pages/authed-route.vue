8bbf3f06f9373868987ad757850fbbbecaf1c58e
Keisuke Shimotori <destrage1958@gmail.com>
2018-11-28 09:38:51 +0900

    3-21

---
+++ chapter3/section21-/pages/authed-route.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>認証が必要なページ</h2>
+    <p>
+      Cookieの'credentil'値がtruthyな場合のみアクセス可能です。<br>
+      <nuxt-link to='/'>トップに戻る</nuxt-link>
+    </p>
+  </div>
+</template>
---
+++ chapter3/section21-/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>index page</h2>
+    <ul>
+      <li>
+        <nuxt-link to='/login'>ログインページ</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to='/authed-route'>認証が必要なページ</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
